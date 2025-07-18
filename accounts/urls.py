0d3b0769ab89214e8862a606c3e4d65f945f2854
michael <michaelokon565@gmail.com>
2025-07-18 08:14:09 +0100

    added login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import RegisterUserView, VerifyUserEmail,LoginUserView, TestAuthenticationView
+
 
 
 
 urlpatterns = [
-    path('register/', RegisterUserView.as_view(), name='register')
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('verify-email/', VerifyUserEmail.as_view(), name='verify'),
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('test/', TestAuthenticationView.as_view(), name='test')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, LoginSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from .utils import send_code_to_user
+from .models import  OneTimePassword
+
 
 
 
@@ -25,3 +28,36 @@ class RegisterUserView(GenericAPIView):
                 'message':f'hi thanks for joining paybill '
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class VerifyUserEmail(GenericAPIView):
+    def post(self, request):
+        otpcode=request.data.get('otp')
+        try:
+            user_code_obj=OneTimePassword.objects.get(code=otpcode)
+            user = user_code_obj.user
+            if not user.is_verified:
+                user.is_verified=True
+                user.save()
+                return Response({'message':'email verified successfully'}, status=status.HTTP_200_OK)
+            return Response({'message':'code is invalid user already verified'}, status=status.HTTP_204_NO_CONTENT)
+            
+        except OneTimePassword.DoesNotExist:
+            return Response({'message':'passcode not provided'}, status=status.HTTP_404_NOT_FOUND)
+        
+
+class LoginUserView(GenericAPIView):
+    serializer_class = LoginSerializer
+    def post(self, request):
+        serializer=self.serializer_class(data=request.data, context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class TestAuthenticationView(GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        data={
+            'msg':'it works'
+        }
+        return Response(data, status=status.HTTP_200_OK)
